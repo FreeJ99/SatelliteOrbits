d82c9036b93e8129babc65e03561956dff433179
freej <jenkoslobodan@gmail.com>
2020-04-20 17:12:07 +0200

    Satellite orbiting with gravity

--- 1.py
+++ 1.py
@@ -7,15 +7,16 @@ G = 6.67408 * 1e-11
 Re = 6371 * 1e3
 M = 5.972 * 1e24
 h = 3000 * 1e3
+dt = 60
 
 viewSize = 3*Re
-
 
 #Initial values
 R = Re + h
 p0 = np.array([R, 0])
-v0 = np.array([np.sqrt(G*M/R), 0])
+v0 = np.array([0, np.sqrt(G*M/R)*1])
 
+print(v0)
 
 #Animation
 fig = plt.figure() 
@@ -28,15 +29,41 @@ def init():
     line.set_data([], []) 
     return line, 
 
-frames = 365
+p = p0
+v = v0
+a = None
+
+def printValues():
+    print('Acceleration: ', a)
+    print('Velocity: ', v)
+    print('Position:', p)
+    print()
+
+def updateValues():
+    global v
+    global p
+    global a
+
+    R = np.linalg.norm(p)
+    nv = np.array([v[1], -v[0]]) / np.linalg.norm([v[1], -v[0]])
+    ps = p/np.linalg.norm(p)
+    a = - ps * G*M/(R**2) #nv * (v**2)/R #problem with numerical precision???
+    #a = np.zeros(2)
+    v += a * dt
+    p += v * dt
+
+    return p
+
 def animate(i):
-    t = (2*np.pi/frames) * i
-    x = R * np.cos(t)
-    y = R * np.sin(t)
-    line.set_data([x], [y]) 
+    if(i%10 == 0):
+        printValues()
+
+    p = updateValues()
+
+    line.set_data([p[0]], [p[1]])
     return line, 
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, 
-                            frames=frames, interval=20, blit=True)
+                            frames=365, interval=40, blit=True)
 
 plt.show()
