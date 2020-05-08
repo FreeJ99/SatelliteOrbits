5d89ac7cfe65f1317ba9a0f1c3fbf38817a03918
freej <jenkoslobodan@gmail.com>
2020-05-08 17:50:57 +0200

    Added experiment results and some real world data

--- Code/graphs.py
+++ Code/graphs.py
@@ -1,8 +1,11 @@
+#TODO moram ispraviti formulu za ro, nije dobra jer sam uzimao A i Cd za sferu, a ne za ISS
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import rcParams
 import time
+import os
 
 rcParams['figure.figsize'] = [12, 9]
 
@@ -14,11 +17,11 @@ Re = 6371 * 1e3
 M = 5.972 * 1e24
 m = 419455 # 419455 for ISS
 h = 4.08 * 1e5#408*1e3 for ISS
-dt = 0.1
+dt = 1
 
 viewSize = 2*Re
 
-Area = 7850 #Sphere with radius = 50m
+Area = 7850 #Sphere with radius = 50m, overkill for ISS
 Cd = 0.47
 
 #Initial values######################################################################
@@ -30,29 +33,34 @@ a = 0
 ro = 6.28e-11
 roGnd = 1.225
 
-#Figures, axes and lines##############################################################
+print(v)
+
+#Figures and axes##############################################################
 figEnergy, axEnergy = plt.subplots()
 figHeight, axHeight = plt.subplots()
 figVelocity, axVelocity = plt.subplots()
 
 axEnergy.set_title("Energy")
-axEnergy.set_xlabel('t')
+axEnergy.set_xlabel('t (min)')
 axEnergy.set_ylabel('E')
 
 axHeight.set_title("Height")
-axHeight.set_xlabel('t')
-axHeight.set_ylabel('h') 
+axHeight.set_xlabel('t (min)')
+axHeight.set_ylabel('h (km)') 
 
 axVelocity.set_title("Velocity")
-axVelocity.set_xlabel('t')
-axVelocity.set_ylabel('v')
+axVelocity.set_xlabel('t (days)')
+axVelocity.set_ylabel('v (m/s)')
 
 Es = []
 Hs = []
 Vs = []
 ts = []
 
-#UPDATING VALUES###########################################################################
+def savePlots(base, figures, paths):
+    for fig, p in zip(figures, paths):
+        fig.savefig(os.path.join(base,p))
+#DEBUGGING###########################################################################
 def printValues():
     print('Acceleration: ', np.linalg.norm(a))
     print('Velocity: ', np.linalg.norm(v))
@@ -61,6 +69,7 @@ def printValues():
     print('t:', t)
     print()
 
+#MAIN LOOP##########################################################################
 h = np.linalg.norm(p) - Re
 while(h > 4.0e5):
     if(roGnd*np.exp(-h/1e4) > ro): #true when h<237km
@@ -84,16 +93,17 @@ while(h > 4.0e5):
     h = np.linalg.norm(p)-Re
 
     Es.append(E)
-    Hs.append(h)
+    Hs.append(h/1e3)
     Vs.append(np.linalg.norm(v))
-    ts.append(t)
+    ts.append(t/(60*60*24))
 
 print("Execution time:", time.time() - program_start_time)
 
 axEnergy.plot(ts, Es, color='lightblue')
 axHeight.plot(ts, Hs, color='lightgreen')
 axVelocity.plot(ts, Vs, color='orange')
-figEnergy.savefig('ISS_E')
-figHeight.savefig('ISS_H')
-figVelocity.savefig('ISS_V')
+
+#savePlots(base = '../Small_dt', figures=[figEnergy, figHeight, figVelocity], 
+#            paths=['ISS_E', 'ISS_H', 'ISS_V'])
+
 plt.show()
